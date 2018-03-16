f21f23c560d8d2f8b23859d153a437ec3aabd4d7
Yiorgis Gozadinos <ggozad@MacBook-Pro.lan>
2018-03-16 18:24:04 +0100

    menumeters

--- brew.sh
+++ brew.sh
@@ -102,7 +102,9 @@ brew cask install --appdir="/Applications" react-native-debugger
 brew cask install --appdir="/Applications" rowanj-gitx
 brew cask install --appdir="/Applications" steam
 brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications" yujitach-menumeters
 brew cask install --appdir="/Applications" crypho
+
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
 
